875a22bdb35341d34c25196095e408dc792ae8ec
N-Tatsuya <136430977+N-Tatsuya@users.noreply.github.com>
2025-02-12 11:38:22 +0900

    1st

---
+++ src/main/java/com/example/Hoteruru3rd/Hoteruru3rdApplication.java
@@ -0,0 +1,13 @@
+package com.example.Hoteruru3rd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hoteruru3rdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hoteruru3rdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Hoteruru3rd/Hoteruru3rdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Hoteruru3rd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hoteruru3rdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
