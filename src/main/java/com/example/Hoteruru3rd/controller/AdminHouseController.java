4b9eb6f8095503bdff6b4e84321703eec4633775
N-Tatsuya <136430977+N-Tatsuya@users.noreply.github.com>
2025-02-13 09:50:39 +0900

    2day

---
+++ src/main/java/com/example/Hoteruru3rd/controller/AdminHouseController.java
@@ -0,0 +1,12 @@
+package com.example.Hoteruru3rd.controller;
+
+@Controller
+@Requestmapping("/admin/houses")
+
+public class AdminHouseController {
+	private final HouseRepository houseRepository;
+	public AdminHouseController(HouseRepository houseRepository) {
+		this.house
+	}
+
+}
---
+++ src/main/java/com/example/Hoteruru3rd/repository/HouseRepository.java
@@ -0,0 +1,9 @@
+package com.example.Hoteruru3rd.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.Hoteruru3rd.entity.House;
+
+public interface HouseRepository extends JpaRepository<House, Integer> {
+
+}
