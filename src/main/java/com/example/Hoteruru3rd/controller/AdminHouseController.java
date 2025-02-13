e93b97d77c8830e8a13b88344d31a67c6390aaa1
test <test@DESKTOP-Q8BHFKN>
2025-02-13 19:03:43 +0900

    2day

--- src/main/java/com/example/Hoteruru3rd/controller/AdminHouseController.java
+++ src/main/java/com/example/Hoteruru3rd/controller/AdminHouseController.java
@@ -1,47 +1,29 @@
 package com.example.Hoteruru3rd.controller;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort.Direction;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.Hoteruru3rd.entity.House;
-import com.example.Hoteruru3rd.repository.HouseRepository;
+import com.example.Hoteruru3rd.form.HouseRegisterForm;
 
 @Controller
 @RequestMapping("/admin/houses")
 public class AdminHouseController {
-	private final HouseRepository houseRepository;
-
-	public AdminHouseController(HouseRepository houseRepository) {
-		this.houseRepository = houseRepository;
-	}
-
-	@GetMapping
-	public String index(Model model,
-			@PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable,
-			@RequestParam(name = "keyword", required = false) String keyword) {
-		Page<House> housePage;
-		if (keyword != null && !keyword.isEmpty()) {
-			housePage = houseRepository.findByNameLike("%" + keyword + "%", pageable);
-		} else {
-			housePage = houseRepository.findAll(pageable);
-		}
-		model.addAttribute("housePage", housePage);
-		model.addAttribute("keyword", keyword);
-		return "admin/houses/index";
-	}
-
+	//======== 中略 ======== 
 	@GetMapping("/{id}")
 	public String show(@PathVariable(name = "id") Integer id, Model model) {
 		House house = houseRepository.getReferenceById(id);
 		model.addAttribute("house", house);
 		return "admin/houses/show";
 	}
+
+	@GetMapping("/register")
+	public String register(Model model) {
+		model.addAttribute("houseRegisterForm", new HouseRegisterForm());
+		return "admin/houses/register";
+	}
 }
+//12.4
