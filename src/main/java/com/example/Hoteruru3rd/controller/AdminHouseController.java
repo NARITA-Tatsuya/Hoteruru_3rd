e380627b09c559509196e92a33e70f7ef81351e4
test <test@DESKTOP-Q8BHFKN>
2025-02-14 00:42:53 +0900

    2day

--- src/main/java/com/example/Hoteruru3rd/controller/AdminHouseController.java
+++ src/main/java/com/example/Hoteruru3rd/controller/AdminHouseController.java
@@ -1,29 +1,35 @@
 package com.example.Hoteruru3rd.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.example.Hoteruru3rd.entity.House;
-import com.example.Hoteruru3rd.form.HouseRegisterForm;
+import com.example.Hoteruru3rd.form.HouseEditForm;
 
 @Controller
 @RequestMapping("/admin/houses")
 public class AdminHouseController {
-	//======== 中略 ======== 
-	@GetMapping("/{id}")
-	public String show(@PathVariable(name = "id") Integer id, Model model) {
-		House house = houseRepository.getReferenceById(id);
-		model.addAttribute("house", house);
-		return "admin/houses/show";
+	//======== 中略 ========  
+	@PostMapping("/{id}/update")
+	public String update(@ModelAttribute @Validated HouseEditForm houseEditForm, BindingResult bindingResult,
+			RedirectAttributes redirectAttributes) {
+		if (bindingResult.hasErrors()) {
+			return "admin/houses/edit";
+		}
+		houseService.update(houseEditForm);
+		redirectAttributes.addFlashAttribute("successMessage", "民宿情報を編集しました。");
+		return "redirect:/admin/houses";
 	}
 
-	@GetMapping("/register")
-	public String register(Model model) {
-		model.addAttribute("houseRegisterForm", new HouseRegisterForm());
-		return "admin/houses/register";
+	@PostMapping("/{id}/delete")
+	public String delete(@PathVariable(name = "id") Integer id, RedirectAttributes redirectAttributes) {
+		houseRepository.deleteById(id);
+		redirectAttributes.addFlashAttribute("successMessage", "民宿を削除しました。");
+		return "redirect:/admin/houses";
 	}
 }
-//12.4
---
+++ src/main/java/com/example/Hoteruru3rd/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.example.Hoteruru3rd.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.Hoteruru3rd.entity.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
